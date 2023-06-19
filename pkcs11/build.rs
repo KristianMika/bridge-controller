e222edc916b30358e08e31e87df415e7f5c7de19
Kristian Mika <kristian.mika@protonmail.com>
2023-06-19 16:50:50 +0200

    feat: generate pkcs#11 bindings

---
+++ pkcs11/build.rs
@@ -0,0 +1,24 @@
+extern crate bindgen;
+
+use std::env::{self};
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-Iheaders")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_file = out_dir.join("bindings.rs");
+    bindings
+        .write_to_file(out_file)
+        .expect("Couldn't write bindings!");
+}
