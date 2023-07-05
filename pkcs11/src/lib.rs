0f97f0e4e536ab098be65ce1f57d734f82d7f8bf
Kristian Mika <kristian.mika@protonmail.com>
2023-07-05 10:38:33 +0200

    feat: add necessary PKCS#11 function signatures

--- pkcs11/src/lib.rs
+++ pkcs11/src/lib.rs
@@ -1,1 +1,218 @@
+use bindings::{
+    CK_ATTRIBUTE_PTR, CK_BBOOL, CK_BYTE_PTR, CK_FLAGS, CK_FUNCTION_LIST_PTR_PTR, CK_MECHANISM_PTR,
+    CK_NOTIFY, CK_OBJECT_HANDLE, CK_OBJECT_HANDLE_PTR, CK_RV, CK_SESSION_HANDLE,
+    CK_SESSION_HANDLE_PTR, CK_SLOT_ID, CK_SLOT_ID_PTR, CK_TOKEN_INFO_PTR, CK_ULONG, CK_ULONG_PTR,
+    CK_USER_TYPE, CK_UTF8CHAR_PTR, CK_VOID_PTR,
+};
+
 mod bindings;
+
+#[no_mangle]
+pub extern "C" fn C_GetFunctionList(ppFunctionList: CK_FUNCTION_LIST_PTR_PTR) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_Initialize(pInitArgs: CK_VOID_PTR) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_GetSlotList(
+    tokenPresent: CK_BBOOL,
+    pSlotList: CK_SLOT_ID_PTR,
+    pulCount: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_GetTokenInfo(slotID: CK_SLOT_ID, pInfo: CK_TOKEN_INFO_PTR) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_OpenSession(
+    slotID: CK_SLOT_ID,
+    flags: CK_FLAGS,
+    pApplication: CK_VOID_PTR,
+    Notify: CK_NOTIFY,
+    phSession: CK_SESSION_HANDLE_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_Login(
+    hSession: CK_SESSION_HANDLE,
+    userType: CK_USER_TYPE,
+    pPin: CK_UTF8CHAR_PTR,
+    ulPinLen: CK_ULONG,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_FindObjectsInit(
+    hSession: CK_SESSION_HANDLE,
+    pTemplate: CK_ATTRIBUTE_PTR,
+    ulCount: CK_ULONG,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_FindObjects(
+    hSession: CK_SESSION_HANDLE,
+    phObject: CK_OBJECT_HANDLE_PTR,
+    ulMaxObjectCount: CK_ULONG,
+    pulObjectCount: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_FindObjectsFinal(hSession: CK_SESSION_HANDLE) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_GetAttributeValue(
+    hSession: CK_SESSION_HANDLE,
+    hObject: CK_OBJECT_HANDLE,
+    pTemplate: CK_ATTRIBUTE_PTR,
+    ulCount: CK_ULONG,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_DestroyObject(hSession: CK_SESSION_HANDLE, hObject: CK_OBJECT_HANDLE) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_CreateObject(
+    hSession: CK_SESSION_HANDLE,
+    pTemplate: CK_ATTRIBUTE_PTR,
+    ulCount: CK_ULONG,
+    phObject: CK_OBJECT_HANDLE_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_GenerateKeyPair(
+    hSession: CK_SESSION_HANDLE,
+    pMechanism: CK_MECHANISM_PTR,
+    pPublicKeyTemplate: CK_ATTRIBUTE_PTR,
+    ulPublicKeyAttributeCount: CK_ULONG,
+    pPrivateKeyTemplate: CK_ATTRIBUTE_PTR,
+    ulPrivateKeyAttributeCount: CK_ULONG,
+    phPublicKey: CK_OBJECT_HANDLE_PTR,
+    phPrivateKey: CK_OBJECT_HANDLE_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_WrapKey(
+    hSession: CK_SESSION_HANDLE,
+    pMechanism: CK_MECHANISM_PTR,
+    hWrappingKey: CK_OBJECT_HANDLE,
+    hKey: CK_OBJECT_HANDLE,
+    pWrappedKey: CK_BYTE_PTR,
+    pulWrappedKeyLen: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_EncryptInit(
+    hSession: CK_SESSION_HANDLE,
+    pMechanism: CK_MECHANISM_PTR,
+    hKey: CK_OBJECT_HANDLE,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_Encrypt(
+    hSession: CK_SESSION_HANDLE,
+    pData: CK_BYTE_PTR,
+    ulDataLen: CK_ULONG,
+    pEncryptedData: CK_BYTE_PTR,
+    pulEncryptedDataLen: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_EncryptUpdate(
+    hSession: CK_SESSION_HANDLE,
+    pPart: CK_BYTE_PTR,
+    ulPartLen: CK_ULONG,
+    pEncryptedPart: CK_BYTE_PTR,
+    pulEncryptedPartLen: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_EncryptFinal(
+    hSession: CK_SESSION_HANDLE,
+    pLastEncryptedPart: CK_BYTE_PTR,
+    pulLastEncryptedPartLen: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_DigestInit(hSession: CK_SESSION_HANDLE, pMechanism: CK_MECHANISM_PTR) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_Digest(
+    hSession: CK_SESSION_HANDLE,
+    pData: CK_BYTE_PTR,
+    ulDataLen: CK_ULONG,
+    pDigest: CK_BYTE_PTR,
+    pulDigestLen: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_DecryptInit(
+    hSession: CK_SESSION_HANDLE,
+    pMechanism: CK_MECHANISM_PTR,
+    hKey: CK_OBJECT_HANDLE,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_Decrypt(
+    hSession: CK_SESSION_HANDLE,
+    pEncryptedData: CK_BYTE_PTR,
+    ulEncryptedDataLen: CK_ULONG,
+    pData: CK_BYTE_PTR,
+    pulDataLen: CK_ULONG_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn C_UnwrapKey(
+    hSession: CK_SESSION_HANDLE,
+    pMechanism: CK_MECHANISM_PTR,
+    hUnwrappingKey: CK_OBJECT_HANDLE,
+    pWrappedKey: CK_BYTE_PTR,
+    ulWrappedKeyLen: CK_ULONG,
+    pTemplate: CK_ATTRIBUTE_PTR,
+    ulAttributeCount: CK_ULONG,
+    phKey: CK_OBJECT_HANDLE_PTR,
+) -> CK_RV {
+    unimplemented!()
+}
