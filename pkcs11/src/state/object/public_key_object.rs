395d63b5a526b80b81b869a2db2fc247660b2ae7
Kristian Mika <kristian.mika@protonmail.com>
2023-08-12 21:37:59 +0200

    refactor: pub key formatting

--- pkcs11/src/state/object/public_key_object.rs
+++ pkcs11/src/state/object/public_key_object.rs
@@ -2,6 +2,8 @@ use crate::cryptoki::bindings::{CKA_EC_POINT, CK_ATTRIBUTE_TYPE};
 
 use super::{object_class::ObjectClass, template::Template, CryptokiObject};
 
+const DER_OCTET_STRING_TAG: u8 = 0x04;
+
 pub(crate) struct PublicKeyObject {
     data: Vec<u8>,
 }
@@ -9,6 +11,14 @@ pub(crate) struct PublicKeyObject {
 impl PublicKeyObject {
     pub(crate) fn new(data: Vec<u8>) -> Self {
         Self { data }
+    }
+
+    fn format_public_key(&self) -> Vec<u8> {
+        let mut public_key = self.data.clone();
+        let data_len = public_key.len();
+        public_key.insert(0, DER_OCTET_STRING_TAG);
+        public_key.insert(1, data_len as u8);
+        public_key
     }
 }
 impl CryptokiObject for PublicKeyObject {
@@ -37,11 +47,8 @@ impl CryptokiObject for PublicKeyObject {
         if attribute_type != CKA_EC_POINT as u64 {
             return None;
         }
-        let mut data = self.data.clone();
-        data.insert(0, 0x04);
-        data.insert(1, 65); // TODO: into a method
 
-        Some(data)
+        Some(self.format_public_key())
     }
 
     fn get_data(&self) -> Vec<u8> {
