507828d2a802d1d8459617f0e3cef6dccd31a2e2
Kristian Mika <kristian.mika@protonmail.com>
2023-08-13 11:15:56 +0200

    test: add integration test

---
+++ pkcs11/integration-tests/run_softfido_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+cleanup() {
+    if [ -n "$softfido_pid" ]; then
+        kill "$softfido_pid"
+    fi
+}
+
+cd softfido && cargo build && cd -
+
+trap 'cleanup' ERR
+
+PKCS11SPY="`pwd`/../target/debug/libmeesign_pkcs11.so" \
+    PKCS11SPY_OUTPUT="`pwd`/pkcs11_spy_log.txt" \
+    ./softfido/target/debug/softfido\
+    --token-label  'Meesign: testgrp' \
+    --pkcs11-module "/usr/lib/pkcs11-spy.so" &
+
+softfido_pid=$!
+
+sudo modprobe vhci-hcd
+sudo usbip attach -r 127.0.0.1 -d 1-1
+
+sleep 1
+
+python3 ./softfido/python/softfido_tests.py
+
+kill "$softfido_pid"
