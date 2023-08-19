2241570b331cc0df0d02bcded1653b435179ef3e
Kristian Mika <kristian.mika@protonmail.com>
2023-08-19 11:05:19 +0200

    refactor: rename key_object.rs

--- pkcs11/src/state/object/mod.rs
+++ pkcs11/src/state/object/mod.rs
@@ -1,18 +1,18 @@
 use std::sync::{Arc, RwLock};
 
 use self::{
-    data_object::DataObject, key_object::SecretKeyObject, private_key_object::PrivateKeyObject,
-    public_key_object::PublicKeyObject, template::Template,
+    data_object::DataObject, private_key_object::PrivateKeyObject,
+    public_key_object::PublicKeyObject, secret_key_object::SecretKeyObject, template::Template,
 };
 use crate::{cryptoki::bindings::CK_ATTRIBUTE_TYPE, state::object::object_class::ObjectClass};
 
 pub(crate) mod attribute;
 pub(crate) mod data_object;
-pub(crate) mod key_object;
 pub(crate) mod object_class;
 pub(crate) mod object_search;
 pub(crate) mod private_key_object;
 pub(crate) mod public_key_object;
+pub(crate) mod secret_key_object;
 pub(crate) mod template;
 
 pub(crate) trait CryptokiObject: Sync + Send {
